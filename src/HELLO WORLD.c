fc2213e8ae56031f83d43dc9b5162561a5ba52b7
jose garibay <71746139+josegaribay552@users.noreply.github.com>
2020-09-29 22:45:22 -0500

    version 1

---
+++ src/HELLO WORLD.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : HELLO.c
+ Author      : JOSE GARIBAY
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+	printf("!!!Hello World!!!");
+	printf("!!!Hello World1!!!");
+	printf("!!!Hello World2!!!");
+	printf("!!!Hello Worl3!!!");
+
+
+	return EXIT_SUCCESS;
+}
